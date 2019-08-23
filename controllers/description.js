8e82486a7103357475d203ff08a267a5b3bedf70
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-08-22 21:20:09 -0400

    add basic request

--- controllers/description.js
+++ controllers/description.js
@@ -8,13 +8,33 @@ const descriptionApi = require('../models/description.js')
 const descriptionRouter = express.Router()
 
 
-descriptionRouter.get('/describecar', (req, res) => {
-  res.render('template/describeCar.hbs', {})
+descriptionRouter.get('/:index', (req,res) => {
+  descriptionApi.getDescription(req.params.index).then(() => {
+    res.send(200)
+  })
+})
+
+descriptionRouter.get('/', (req,res) => {
+  descriptionApi.getDescriptions().then(() => {
+    res.send(200)
+  })
 })
 
 descriptionRouter.post('/', (req,res) => {
-  descriptionApi.addDescription(req.body).then(() => {
-    res.send('Post listing')
+  descriptionApi.addDescription().then(() => {
+    res.send(200)
+  })
+})
+
+descriptionRouter.put('/:index', (req,res) => {
+  descriptionApi.editDescription().then(() => {
+    res.send(200)
+  })
+})
+
+descriptionRouter.delete('/:index', (req,res) => {
+  descriptionApi.deleteDescription().then(() => {
+    res.send(200)
   })
 })
 
