e41f748232e9dbdf7e556e2691f45b8853fbb4c9
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-08-22 21:29:58 -0400

    added base request

--- controllers/user.js
+++ controllers/user.js
@@ -8,14 +8,35 @@ const userApi = require('../models/user.js')
 const userRouter = express.Router()
 
 
-userRouter.get('/', (req, res) => {
-  res.render('template/homePage')
+userRouter.get('/:index', (req,res) => {
+  userApi.findUser(req.params.index).then(() => {
+    res.send(200)
+  })
 })
 
-userRouter.get('/addyourself', (req,res) => {
-  res.render('template/addYourself', {})
+userRouter.get('/', (req,res) => {
+  userApi.findUsers().then(() => {
+    res.send(200)
+  })
 })
 
+userRouter.post('/', (req,res) => {
+  userApi.addUser().then(() => {
+    res.send(200)
+  })
+})
+
+userRouter.put('/:index', (req,res) => {
+  userApi.editUser(req.params.index, req.body).then(() => {
+    res.send(200)
+  })
+})
+
+userRouter.delete('/:index', (req,res) => {
+  userApi.deleteUser(req.params.index).then(() => {
+    res.send(200)
+  })
+})
 
 
 
