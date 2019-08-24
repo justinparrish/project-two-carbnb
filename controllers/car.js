bacdc99751f229fe1fe6d4d0a173e6d043533f58
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-08-24 12:15:19 -0400

    changed from _id to id

--- controllers/car.js
+++ controllers/car.js
@@ -2,54 +2,53 @@
 const express = require('express')
 
 const carApi = require('../models/car.js')
-const userApi = require('../models/user.js')
 
 
-const userRouter = express.Router()
 const carRouter = express.Router()
 
 
 
-carRouter.get('/carlisting', (req,res) => {
-  res.render('template/carListing')
-})
-
-carRouter.get('/:id', (req,res) => {
-  carApi.getCar(req.params.id).then(() => {
-    res.send(200)
-  })
-})
+// carRouter.get('/carlisting', (req,res) => {
+//   res.render('template/carListing')
+// })
 
 
 
 
-carRouter.get('/', (req,res) => {
+
+
+carRouter.get('/carsListing', (req,res) => {
   carApi.getCars().then((allCars) => {
+    console.log(allCars)
     res.render('template/carListing', {allCars})
   })
 })
 
-carRouter.post('/', (req,res) => {
-  carApi.addCar(req.body).then(() => {
+
+//to get car 
+carRouter.get('/:vinNum', (req,res) => {
+  carApi.getCar(req.params.vinNum).then(oneCar => {
+    res.render('template/editCar', {oneCar, vinNum: req.params.vinNum})
+  })
+})
+
+
+carRouter.post('/carsListing', (req,res) => {
+  carApi.createCar(req.body).then(() => {
     res.render('template/carListing')
   })
 })
 
-userRouter.post('/cars/carListing', (req,res) => {
-  userApi.addUser(req.body).then(() => {
-    res.render('template/carListing')
+
+carRouter.put('/:vinNum', (req,res) => {
+  carApi.editCar(req.params.vinNum, req.body).then(() => {
+    res.render('/template/carListing')
   })
 })
 
-carRouter.put('/:id', (req,res) => {
-  carApi.editCar(req.params.id, req.body).then(() => {
-    res.send(200)
-  })
-})
-
-carRouter.delete('/:id', (req,res) => {
-  carApi.deleteCar(req.params.id).then(() => {
-    res.send(200)
+carRouter.delete('/:vinNum', (req,res) => {
+  carApi.deleteCar(req.params.vinNum).then(() => {
+    res.render('template/homePage')
   })
 })
 
@@ -59,6 +58,5 @@ carRouter.delete('/:id', (req,res) => {
 
 
 module.exports = {
-  carRouter,
-  userRouter
+  carRouter
 }
