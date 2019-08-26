86e1f61591a00482245249e4621c0d66a40495d4
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-08-25 20:35:44 -0400

    fixed all relevant bugs in description controller

--- controllers/description.js
+++ controllers/description.js
@@ -8,16 +8,10 @@ const descriptionApi = require('../models/description.js')
 const descriptionRouter = express.Router()
 
 
-descriptionRouter.get('/addDescript', (req,res) => {
-  res.render('template/description/addDescript')
+descriptionRouter.get('/adddescript', (req,res) => {
+  res.render('template/description/addDescript', {})
 })
 
-
-descriptionRouter.get('/:descriptId', (req,res) => {
-  descriptionApi.getDescription(req.params.descriptId).then(oneD => {
-    res.render('/template/description/editeDescript', {oneD, descriptId: req.params.descriptId})
-  })
-})
 
 descriptionRouter.get('/search', (req,res) => {
   descriptionApi.getDescriptions().then((allDescripts) => {
@@ -26,6 +20,14 @@ descriptionRouter.get('/search', (req,res) => {
     res.render('template/description/descriptSearch', {allDescripts})
   })
 })
+
+
+descriptionRouter.get('/:descriptId', (req,res) => {
+  descriptionApi.getDescription(req.params.descriptId).then(oneD => {
+    res.render('template/description/editDescript', {oneD, descriptId: req.params.descriptId})
+  })
+})
+
 
 descriptionRouter.post('/search', (req,res) => {
   descriptionApi.addDescription(req.body).then(() => {
